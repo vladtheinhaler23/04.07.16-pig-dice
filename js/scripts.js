e04253d702120c76244fdd15ef833b97912af468
Anthony Gold and Josh Logan <anthony@>
2016-04-06 14:18:02 -0700

    has a fxn that returns the grand total but doesnt log it right

--- js/scripts.js
+++ js/scripts.js
@@ -4,32 +4,55 @@ function roll() {
   return Math.floor((Math.random() * 6) + 1);
 }
 
-var turnTotal = 0;
+function Player(name, turn, total) {
+  this.name = name;
+  this.turn = turn;
+}
 
-
-
+Player.prototype.grandTotal = function(turnTotal) {
+  this.grandTotal += turnTotal;
+}
 //USERINTERFACE
 
 $(document).ready(function() {
+
+  var turnTotal = 0;
+  var anthony = new Player("#", turnTotal);
+
   $("#rollDice").click(function(event) {
     event.preventDefault();
 
     var diceRoll = roll();
 
 
+
     if (diceRoll != 1) {
       var count = turnTotal += diceRoll;
       turnTotal = count;
     } else {
-      alert("Sorry, you rolled a 1");
       turnTotal = 0;
     };
 
     console.log(diceRoll);
     console.log(turnTotal);
 
-
   })
+
+  $("#hold").click(function(event) {
+      event.preventDefault();
+
+      if (turnTotal === 0) {
+        alert("Please roll the dice.");
+      } else {
+        anthony.grandTotal(turnTotal);
+      }
+
+    console.log(anthony.grandTotal);
+
+
+    })
+
+
 
 
 })
