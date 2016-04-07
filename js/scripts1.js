d1a4b993daba435b15a60132c7cdb3ac1083cf4d
Anthony Gold and Joshua Logan <anthony@>
2016-04-07 09:54:03 -0700

    added functionality for a second die, started on user interface

--- js/scripts1.js
+++ js/scripts1.js
@@ -9,8 +9,8 @@ Score.prototype.addTotal = function(turnScore) {
   return this.totalScore + turnScore;
 }
 
-Score.prototype.addTurn = function(roll) {
-  return this.turnScore + roll;
+Score.prototype.addTurn = function(roll1, roll2) {
+  return this.turnScore + roll1 + roll2;
 }
 // User Interface
 $(document).ready(function() {
@@ -18,30 +18,40 @@ $(document).ready(function() {
   var player = 1;
   var player1 = new Score();
   var player2 = new Score();
-  var roll = 0;
+  var roll1 = 0;
+  var roll2 = 0;
 
     $("#rollDice").click(function(event) {
 
-      roll = Math.floor((Math.random() * 6) + 1)
+      roll1 = Math.floor((Math.random() * 6) + 1)
+      roll2 = Math.floor((Math.random() * 6) + 1)
 
       if (player === 1) {
-        if (roll === 1) {
+        if (roll1 === 1 && roll2 === 1) {
+          player1.totalScore = 0;
+          player = 2;
+        } else if (roll1 === 1 || roll2 === 1) {
           player1.turnScore = 0;
           player = 2;
         } else {
-          player1.turnScore = player1.addTurn(roll);
+          player1.turnScore = player1.addTurn(roll1, roll2);
         }
       } else {
-        if (roll === 1) {
+        if (roll1 === 1 && roll2 === 1)  {
+          player2.totalScore = 0;
+          player = 1;
+        } else if (roll1 === 1 || roll2 === 1) {
           player2.turnScore = 0;
           player = 1;
         } else {
-          player2.turnScore = player2.addTurn(roll);
+          player2.turnScore = player2.addTurn(roll1, roll2);
         }
       }
-      console.log("your roll:", roll);
+      console.log("your roll:", roll1, roll2);
       console.log("player1 turn score:", player1.turnScore);
       console.log("player2 turn score:", player2.turnScore);
+      console.log("player 1 total score:", player1.totalScore);
+      console.log("player 2 total score:", player2.totalScore);
     });
 
     $("#hold").click(function(event) {
